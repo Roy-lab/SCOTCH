e70cfb3e76831aa5e4f81dd0db5e79266be13425
Spencer Halberg <shalberg@wisc.edu>
2022-11-04 11:22:01 -0500

    Added convertTextToPT.py function. Use to change a text file into a matrix tensor file. Drastically improves load in spead an overall performance.

--- convertTextToPT.py
+++ convertTextToPT.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse 
 import os
 
+
 def main(args):
     file_parts = os.path.splitext(args.in_file)
     df = pd.read_csv(args.in_file, sep=args.delimiter, header=args.header, dtype=args.dtype)
@@ -27,7 +28,7 @@ if __name__ == "__main__":
                         help="Number of lines before data. Default is zero",
                         required = False,
                         default = None)
-    parser.add_argument('--data_type',
+    parser.add_argument('--dtype',
                         help="Data type for .pt file. Default is np.float32",
                         required=False,
                         default=np.float32)
